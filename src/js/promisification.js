4ce1566d6c5a7adb4e5f38026fbd456e8315c481
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2020-12-18 21:31:17 +0200

    Дз№3
    Решены 3 задачи.

---
+++ src/js/promisification.js
@@ -0,0 +1,111 @@
+
+const delay = ms => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(ms)
+      },ms)
+  })
+};
+
+
+
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
+
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+
+    return new Promise((resolve) => {
+        const updatedUsers = allUsers.map(user => {
+            if (user.name === userName) {
+                resolve(user)
+            }           
+        })
+    })
+}
+
+
+const loger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+toggleUserState(users, 'Mango', loger);
+toggleUserState(users, 'Lux', loger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(loger);
+toggleUserState(users, 'Lux').then(loger);
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction) => {
+    return new Promise((resolve, reject) => {
+        const delay = randomIntegerFromInterval(200, 500);
+
+        setTimeout(() => {
+            const canProcess = Math.random() > 0.3;
+
+            if (canProcess) {
+                resolve(transaction.id, delay);
+      
+            } else {
+                reject(transaction.id)
+      
+            }
+        }, delay);
+
+               
+    })
+    
+};
+  const logSuccess = (id, time) => {
+            console.log(`Transaction ${id} processed in ${time}ms`);
+        };
+
+        const logError = id => {
+            console.warn(`Error processing transaction ${id}. Please try again later.`);
+        };
+
+/*
+ * Работает так
+ */
+makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
